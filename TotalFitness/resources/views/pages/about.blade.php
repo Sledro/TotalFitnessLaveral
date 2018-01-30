bc52adb56c8acdf66c79d0cbace4b795008fb23e
sledro <dan@sledro.com>
2018-01-30 13:12:12 +0000

    Database migration setup. Posts table added. Custom CSS added.

--- TotalFitness/resources/views/pages/about.blade.php
+++ TotalFitness/resources/views/pages/about.blade.php
@@ -3,10 +3,10 @@
 @section('content')
     <h1>This is the about page</h1>
     <p>Thanks for viewing my about page.</p>
-    <ul>
+    <ul class="list-group">
     @if(count($services) > 0)
         @foreach($services as $service)
-            <li>{{$service}}</li>
+            <li class="list-group-item">{{$service}}</li>
         @endforeach
     @endif
     </ul>
