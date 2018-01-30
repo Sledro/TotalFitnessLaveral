ccf831a2e4dc114fd14bad8b23f28e91e28d070c
sledro <dan@sledro.com>
2018-01-30 00:01:22 +0000

    PagesContorller added, routing set up.

---
+++ TotalFitness/resources/views/pages/about.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{config('app.name', 'Total Fitness')}}</title>
+
+    </head>
+    <body>
+    <h1>This is the about page</h1>
+    <p>Thanks for viewing my about page.</p>
+    </body>
+</html>
