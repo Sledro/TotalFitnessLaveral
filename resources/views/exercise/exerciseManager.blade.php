a4e40299295d4470816a6514aeaf684d59f177c5
Sledro <dan@sledro.com>
2018-04-13 13:14:53 +0100

    Table added to view.

--- resources/views/exercise/exerciseManager.blade.php
+++ resources/views/exercise/exerciseManager.blade.php
@@ -1,9 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container-fluid">
-	<div class="row">
-	
-	</div>
+        <div class="card">
+        <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">Plan ID</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Created</th>
+                    <th scope="col">Manage</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Beginner</td>
+                    <td>11:11 23/03/33</td>
+                    <td><a href="#" class="btn btn-primary" style="float:left;margin-right:10px;">Manage</a></td>
+                  </tr>
+                </tbody>
+              </table>
+    </div>         
 </div>
 @endsection
