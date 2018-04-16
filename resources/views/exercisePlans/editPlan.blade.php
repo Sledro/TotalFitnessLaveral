657ecba22b70a63693759a0b5c33a1451b14df24
Sledro <dan@sledro.com>
2018-04-16 14:30:54 +0100

    Collapasable UI added.

--- resources/views/exercisePlans/editPlan.blade.php
+++ resources/views/exercisePlans/editPlan.blade.php
@@ -2,29 +2,126 @@
 
 @section('content')
     <h1>Edit Training Plan</h1>
-
     {!! Form::open(['action' => ['ExercisePlansController@update', $plan->id], 'method' => 'POST']) !!}
+    {{ Form::hidden('_method', 'PUT') }}
+    {{ Form::submit('Update Plan', ['class' => 'btn btn-primary']) }}
+    {{ Form::submit('Delete Plan', ['class' => 'btn btn-danger']) }}
     <div class="form-group">
             {{ Form::label('title', 'Name') }}
             {{ Form::text('title', $plan->name, ['class' => 'form-control', 'placeholder' => 'title']) }}
     </div>
 
+    <div class="col-md-2" style="padding:0px;flex:0%;">
+        <div class="card" style="padding:10px;background-color:#c7e0fc;">
+            <h3 style="margin-bottom:3px;">
+                <center>Monday</center>
+                <hr style="margin:10px;"></hr>
+            </h3>
 
-    <!--Iterate through exercises in the plan!-->  
-    @foreach($plan->exercises as $exercise)
-    <div class="card"> 
-      <div class="form-group">    
-          {{ Form::label('exerciseList', 'Exercise') }} <br/>
-          {{ Form::select('exerciseList', $exerciseList) }}
-      </div>
-      <div class="form-group">    
-          {{$exercise->reps}}
-      </div>
+            <!--Iterate through exercises in the plan!-->  
+            @foreach($plan->exercises as $exercise)
+                @if($exercise->day=="Monday")
+                    <div class="panel-group" id="accordion">
+                            <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <?$ran=rand(10,10000)?>
+                                <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$ran}}">
+                                    <!--Iterate through exercises in user plans for Monday!-->  
+                                    @foreach($exercise->exercise as $exercises)
+                                        {{$exercises->name}}</a>
+                                    @endforeach
+                                </h4>
+                            </div>
+                            <div id="collapse{{$ran}}" class="collapse">
+                                <div class="panel-body">
+                                    <div class="card" style="padding:10px;"> 
+                                        <div class="form-group">    
+                                            {{ Form::label('exerciseList', 'Exercise') }} <br/>
+                                            {{ Form::select('exerciseList', $exerciseList) }} <br/>
+                                        </div>
+                                        <div class="form-group">    
+                                                {{ Form::label('description', 'Exercise Description') }} <br/>
+                                                <a class="card-link"  href="./exercise/{{$exercise->id}}">Exercise Details</a><br/>
+                                        </div>
+                                        <div class="form-group">    
+                                                {{ Form::label('reps', 'Number of Reps') }} <br/>
+                                                {{ Form::text('reps', $exercise->reps) }} <br/>
+                                        </div>
+                                        <div class="form-group">    
+                                                {{ Form::label('sets', 'Number of Sets') }} <br/>
+                                                {{ Form::text('sets', $exercise->sets) }} <br/>
+                                        </div>
+                                        <div class="form-group">    
+                                                {{ Form::label('weight', 'Weight (kg)') }} <br/>
+                                                {{ Form::text('weight', $exercise->weight) }} <br/>
+                                        </div>     
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endif
+            @endforeach
+        </div>
     </div>
-    @endforeach
 
-    {{ Form::hidden('_method', 'PUT') }}
-    {{ Form::submit('Update Plan', ['class' => 'btn btn-primary']) }}
+    <div class="col-md-2" style="padding:0px;flex:0%;">
+            <div class="card" style="padding:10px;background-color:#c7e0fc;">
+                <h3 style="margin-bottom:3px;">
+                    <center>Monday</center>
+                    <hr style="margin:10px;"></hr>
+                </h3>
+    
+                <!--Iterate through exercises in the plan!-->  
+                @foreach($plan->exercises as $exercise)
+                    @if($exercise->day=="Monday")
+                        <div class="panel-group" id="accordion">
+                                <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion" href="#collapse{{$loop->iteration}}">
+                                        <!--Iterate through exercises in user plans for Monday!-->  
+                                        @foreach($exercise->exercise as $exercises)
+                                            {{$exercises->name}}</a>
+                                        @endforeach
+                                    </h4>
+                                </div>
+                                <div id="collapse{{$loop->iteration}}" class="collapse">
+                                    <div class="panel-body">
+                                        <div class="card" style="padding:10px;"> 
+                                            <div class="form-group">    
+                                                {{ Form::label('exerciseList', 'Exercise') }} <br/>
+                                                {{ Form::select('exerciseList', $exerciseList) }} <br/>
+                                            </div>
+                                            <div class="form-group">    
+                                                    {{ Form::label('description', 'Exercise Description') }} <br/>
+                                                    <a class="card-link"  href="./exercise/{{$exercise->id}}">Exercise Details</a><br/>
+                                            </div>
+                                            <div class="form-group">    
+                                                    {{ Form::label('reps', 'Number of Reps') }} <br/>
+                                                    {{ Form::text('reps', $exercise->reps) }} <br/>
+                                            </div>
+                                            <div class="form-group">    
+                                                    {{ Form::label('sets', 'Number of Sets') }} <br/>
+                                                    {{ Form::text('sets', $exercise->sets) }} <br/>
+                                            </div>
+                                            <div class="form-group">    
+                                                    {{ Form::label('weight', 'Weight (kg)') }} <br/>
+                                                    {{ Form::text('weight', $exercise->weight) }} <br/>
+                                            </div>     
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endforeach
+            </div>
+        </div>
+
+
     {!! Form::close() !!}
-
+        
+   
 @endsection
